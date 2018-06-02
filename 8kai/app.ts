a5c34aefd1ad056a2f632638acec1bffce1cddfa
Caffeintegral <kazusaichinomiya2220@gmail.com>
2018-06-02 22:32:47 +0900

    CG演習8回

---
+++ 8kai/app.ts
@@ -0,0 +1,140 @@
+///<reference path="./node_modules/@types/three/index.d.ts"/>
+///<reference path="./node_modules/@types/dat-gui/index.d.ts"/>
+
+class ThreeJSTest {
+    private scene: THREE.Scene;
+    private camera: THREE.Camera;
+    private renderer: THREE.WebGLRenderer;
+    private geometry: THREE.Geometry;
+    private shapeGeometry: THREE.Geometry;
+    private material: THREE.Material;
+    private material1: THREE.Material;
+    private material2: THREE.Material;
+    private cube: THREE.Mesh;
+    private light: THREE.Light;
+    private screenWidth: number = 640;
+    private screenHeight: number = 480;
+    private controls: GuiControl;
+    private orbitControl: THREE.OrbitControls;
+    private shape1: THREE.Object3D;
+
+    constructor() {
+        this.createRenderer();
+        this.createScene();
+        this.drawShape();
+        this.createShape();
+        
+    } 
+
+    private createRenderer() {
+        // dat.GUI
+        var gui = new dat.GUI({ autoPlace: false, width: 256 });
+        var guielement = document.createElement("div");
+        guielement.id = "dat-gui";
+        guielement.appendChild(gui.domElement);
+        document.getElementById("viewport").appendChild(guielement);
+
+
+        this.controls = new GuiControl();
+//        var gui = new dat.GUI();
+        gui.add(this.controls, 'rotationSpeed', 0, 0.5);
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(this.screenWidth, this.screenHeight);
+        this.renderer.setClearColor(new THREE.Color(0x495ed));
+        document.getElementById("viewport").appendChild(this.renderer.domElement);
+
+
+
+
+    }  
+
+    public drawShape(){
+        var shape = new   THREE.Shape();
+        // スタート地点へ移動
+        shape.moveTo(10,10);
+        // 線を描く
+        shape.lineTo(10,40);
+        //2次のカーブを描く
+        shape.quadraticCurveTo(20,50,30,40);
+
+
+        //Spline曲線を描く
+        shape.splineThru([new THREE.Vector2(32,30), new THREE.Vector2(20,20), new THREE.Vector2(25,10)]);
+
+        
+
+        return shape;
+    }
+
+    public createShape(){
+        var options = {
+            amount: 10,
+            bevelThickness: 2,
+            bevelSize: 1,
+            bevelSegments: 3,
+            bevelEnabled: true,
+            curveSegments: 12,
+            steps: 1
+        };
+        var shapeGeometry = new THREE.ExtrudeGeometry(this.drawShape(), options)
+        var materials = [new THREE.MeshNormalMaterial({side:THREE.DoubleSide}),new THREE.MeshBasicMaterial({wireframe:true})];
+        this.shape1  = THREE.SceneUtils.createMultiMaterialObject(shapeGeometry,materials);
+        this.scene.add(this.shape1); 
+
+    }
+
+    private createScene() {
+        this.scene = new THREE.Scene();
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshLambertMaterial({ color: 0x55ff00 });
+        this.cube = new THREE.Mesh(this.geometry, this.material);
+        // this.scene.add(this.cube);
+        this.camera = new THREE.PerspectiveCamera(75, this.screenWidth /   
+        this.screenHeight, 0.1, 1000);
+        this.camera.position.x = 50;
+        this.camera.position.y = 50;
+        this.camera.position.z = 50;
+        this.camera.lookAt(new THREE.Vector3(0,0,0));
+        
+        
+               
+        
+        this.orbitControl = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.light = new THREE.DirectionalLight(0xffffff);
+        var lvec = new THREE.Vector3(1, 1, 1).normalize();
+        this.light.position.set(lvec.x, lvec.y, lvec.z);
+        this.scene.add(this.light);
+
+
+
+    } 
+
+    public render() {
+//        this.cube.rotation.x += 0.02;
+//        this.cube.rotation.y += 0.02;
+        this.shape1.rotation.x += this.controls.rotationSpeed;
+        this.shape1.rotation.y += this.controls.rotationSpeed;
+
+        this.orbitControl.update();
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.render.bind(this));
+
+
+    }
+
+}
+
+class GuiControl {
+    public rotationSpeed: number;
+    constructor() {
+       this.rotationSpeed = 0.01;
+    }
+}
+
+
+
+window.onload = () => {
+    var threeJSTest = new ThreeJSTest();
+    threeJSTest.render(); 
+};
